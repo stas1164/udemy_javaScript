1a131fc222221e803f341e0e136795173a7ac0d4
stas <sa.ysroslavtsev@gmail.com>
2019-11-19 22:52:47 +1100

    Задание №3 первый коммит

---
+++ les3/script.js
@@ -0,0 +1,60 @@
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц","");
+    time = prompt("Введите дату в формате YYYY-MM-DD","");
+    while(isNaN(money) || money == ""|| money == null) {
+        money = +prompt("Ваш бюджет на месяц","");
+    }
+}
+start();
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: true 
+};
+
+function chooseExpenses() {
+// 1 - цикл for
+for (let i=0; i<2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "" ),
+        b = prompt("Во сколько обойдется?", "" );   
+    if ((typeof(a))==='string' && (typeof(a))!=null && (typeof(b))!=null 
+        && a!='' && b!='' && a.length <50){
+        console.log('done');     
+        appData.expenses[a] = b;
+
+    }else{
+        i=0; 
+    }
+}
+}
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget /30).toFixed(1);
+alert('Ежедневный бюджет' + appData.moneyPerDay);
+
+if(appData.moneyPerDay <100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay<2000) {
+    console.log("средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+
+function checkSavings(){
+    if (appData.savings ==true) {
+        let save = +prompt("Какова сумма накоплений"),
+            persent = +prompt("Под какой процент");
+        
+            appData.monthIncome = save/100/12*persent;
+            alert ("Доход в месяц с вашего депозита:" +appData.monthIncome);
+    }
+}
+checkSavings();
