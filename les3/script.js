bcb46b8e1913b49ae5666d95371caed42a6b53cb
stas <sa.ysroslavtsev@gmail.com>
2019-11-20 22:42:59 +1100

    Выполнено задание 3 (Функции)

--- les3/script.js
+++ les3/script.js
@@ -19,7 +19,7 @@ let appData = {
 };
 
 function chooseExpenses() {
-// 1 - цикл for
+
 for (let i=0; i<2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", "" ),
         b = prompt("Во сколько обойдется?", "" );   
@@ -35,9 +35,13 @@ for (let i=0; i<2; i++) {
 }
 chooseExpenses();
 
+let detectDayBudget = function () { 
 appData.moneyPerDay = (appData.budget /30).toFixed(1);
 alert('Ежедневный бюджет' + appData.moneyPerDay);
+}
+detectDayBudget();
 
+function detectLevel() {
 if(appData.moneyPerDay <100) {
     console.log("Минимальный уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay<2000) {
@@ -47,6 +51,8 @@ if(appData.moneyPerDay <100) {
 } else {
     console.log("Произошла ошибка");
 }
+}
+detectLevel();
 
 function checkSavings(){
     if (appData.savings ==true) {
@@ -58,3 +64,16 @@ function checkSavings(){
     }
 }
 checkSavings();
+
+function chooseOptExpenses() {
+    for (let i=1; i<=3; i++) {
+        let a = prompt("Статья необязательных расходов", "" );
+        if ((typeof(a))==='string' && (typeof(a))!=null && a.length <50){
+            console.log('doneOptEx');     
+            appData.optionalExpenses [i] = a;
+        }else{
+            i=0; 
+        }
+    }
+}
+chooseOptExpenses();
