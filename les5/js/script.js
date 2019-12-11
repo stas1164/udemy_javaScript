f22d015db46bfab4eeee3db58b80bd46e99e4341
Ярославцев Станислав Алексеевич <s.yaroslavtsev@ASO.RT.LOCAL>
2019-12-11 11:00:59 +1100

    выполнение задания 6

--- les5/js/script.js
+++ les5/js/script.js
@@ -1,3 +1,24 @@
-let list = document.querySelector('.menu'); 
-// console.log(list); 
- list.removeChild(list[2]);
+let 
+list = document.querySelector('.menu'),
+title = document.getElementById('title'),
+feedBack = document.getElementById('prompt'),
+adv = document.querySelector('.adv');
+
+list.innerHTML = `<li class="menu-item">Первый пункт</li>
+                   <li class="menu-item">Второй пункт</li>
+                   <li class="menu-item">Третий пункт</li>
+                   <li class="menu-item">Четвёртый пункт</li>
+                   <li class="menu-item">Пятый пункт</li>`
+
+document.body.style.background= "url(img/apple_true.jpg)";
+
+title.textContent = 'Мы продаём только подлинную технику Apple';
+
+adv.remove();
+
+question=prompt('Вопрос');
+if (typeof feedBack.innerText !== 'undefined') {
+    feedBack.textContent=question;
+} else {
+    feedBack.textContent='Неопределено';
+};
